b681634c42496a5923d7c248d5709d41f3fd578c
unknown <noah13wotring@outlook.com>
2020-09-22 22:34:23 -0400

    rearranged remote directory

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join('C:/Users/Noah Wotring/Desktop/Code/portfolio/', 'build');
+const publicPath = path.join(__dirname,'..', 'build');
 app.use(express.static(publicPath));
 
 app.get('*',(req,res) => {
