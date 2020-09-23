c8b6402739464db2902dc30be0299df113f0987b
unknown <noah13wotring@outlook.com>
2020-09-22 21:11:57 -0400

    setting up express server

---
+++ build/precache-manifest.6b02deb531c4805e02a5fc28c51c24d7.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef0301122e35d8f3bea3dff4558c150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d905880d10b09183832d",
+    "url": "/static/css/main.37604972.chunk.css"
+  },
+  {
+    "revision": "64304b7e67686df63f5f",
+    "url": "/static/js/2.6e3bcda9.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.6e3bcda9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d905880d10b09183832d",
+    "url": "/static/js/main.c194b0ff.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "60925c9ed0f56b29bfd5f4e881e3ee3b",
+    "url": "/static/media/a4.60925c9e.wav"
+  },
+  {
+    "revision": "3c4b626482e6417a7f5a229375ac56a7",
+    "url": "/static/media/apocbackground.3c4b6264.jpg"
+  },
+  {
+    "revision": "cbedeaf1800495a9a75c6d5661f1a5d8",
+    "url": "/static/media/b4.cbedeaf1.wav"
+  },
+  {
+    "revision": "798a2ce527cbcd0c0f95c6496e90fdbd",
+    "url": "/static/media/c4.798a2ce5.wav"
+  },
+  {
+    "revision": "7561cda5751f326a3a35b125037a6c03",
+    "url": "/static/media/c5.7561cda5.wav"
+  },
+  {
+    "revision": "3a2fac44f30e8abfd7b769374fecee81",
+    "url": "/static/media/colorplay.3a2fac44.png"
+  },
+  {
+    "revision": "ea989c15569bfc04a2a43d12856ef0c0",
+    "url": "/static/media/d4.ea989c15.wav"
+  },
+  {
+    "revision": "b71bdf91da0a7731a2ef840d510c4913",
+    "url": "/static/media/e4.b71bdf91.wav"
+  },
+  {
+    "revision": "7518cdc29764c336d6af8a35a75c41ac",
+    "url": "/static/media/experience.7518cdc2.png"
+  },
+  {
+    "revision": "28626326e8fc7de71aa58266f1d9d3c1",
+    "url": "/static/media/f4.28626326.wav"
+  },
+  {
+    "revision": "bea9adfe01f71ee4c7f778c6222e1007",
+    "url": "/static/media/fancyN.bea9adfe.jpg"
+  },
+  {
+    "revision": "fc05396c127c9f6b0807f4a5f0de674f",
+    "url": "/static/media/fart.fc05396c.wav"
+  },
+  {
+    "revision": "6b79a5b9ff717d4ce3b6d171f135d2dd",
+    "url": "/static/media/g4.6b79a5b9.wav"
+  },
+  {
+    "revision": "bbb694b3f99cb02898ee4e9d90f97fc3",
+    "url": "/static/media/logo.bbb694b3.png"
+  },
+  {
+    "revision": "fa1ba7222a917e67d15a21b6f8affd2a",
+    "url": "/static/media/rep_grimace.fa1ba722.jpg"
+  },
+  {
+    "revision": "29c75eafda184653882f65d572f692b3",
+    "url": "/static/media/rep_joe.29c75eaf.png"
+  },
+  {
+    "revision": "fdf3eb3a6e07412ff9ccbc0859705b0f",
+    "url": "/static/media/rep_slug.fdf3eb3a.png"
+  },
+  {
+    "revision": "b5452200d1f28a92aa37631373b94919",
+    "url": "/static/media/rep_tobs.b5452200.png"
+  },
+  {
+    "revision": "679fe8ce7abf9359c87f5ee0dd5184a3",
+    "url": "/static/media/simonsings.679fe8ce.png"
+  },
+  {
+    "revision": "b62036189d253032329b2e05560f4295",
+    "url": "/static/media/viewbali.b6203618.jpg"
+  },
+  {
+    "revision": "7597121ebc7080cc190f08a0ee5c7e14",
+    "url": "/static/media/viewhawaii.7597121e.jpg"
+  },
+  {
+    "revision": "2a87ec42bdc173a38ff0f8de577dcb01",
+    "url": "/static/media/viewhotel.2a87ec42.jpg"
+  },
+  {
+    "revision": "0736460b6e172e97f146f64729d315c9",
+    "url": "/static/media/viewmountain.0736460b.jpg"
+  },
+  {
+    "revision": "9e77b06d0bff5f0f68dece8f8ae4db28",
+    "url": "/static/media/viewsunset.9e77b06d.jpg"
+  },
+  {
+    "revision": "c7eb491b7aa7ee24af245906a72384de",
+    "url": "/static/media/white2.c7eb491b.jpg"
+  },
+  {
+    "revision": "376a8984dc2b33bb8fb1c58db74be7d6",
+    "url": "/static/media/zombiemath.376a8984.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b08719c0820cdf774ed8206ba5e30ea.js"
+  "/precache-manifest.6b02deb531c4805e02a5fc28c51c24d7.js"
 );
 
 self.addEventListener('message', (event) => {
